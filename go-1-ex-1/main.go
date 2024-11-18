6809c9ff8d7a6b00da8e8f84543e6bafbdbd0282
LEEEAAAAAA <lea.schuerch@gmail.com>
2024-11-18 09:29:47 +0100

    Till Aufgabe 2

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,9 +4,19 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	var firstName string = "Lea"
+	var lastName string = "Schürch"
+	var dayOfBirth int = 24
+	var monthOfBirth int = 6
+	var yearOfBirth int = 2007
+	var numberOfSiblings int = 1
+	var heightInMeters float64 = 2.0
+	var zodiacSign string = "Cancer" 
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
 	fmt.Printf("Grösse (in Metern): %.2f\n", heightInMeters)
-	fmt.Printf("Sternzeichen: %c\n", zodiacSign)
+	fmt.Printf("Sternzeichen: %s\n", zodiacSign)
 }
+
--- go-1-ex-3/main.go
+++ go-1-ex-3/main.go
@@ -4,18 +4,27 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"os"
 )
 
 func main() {
 	var eyes = rand.Intn(5) + 1
 	var when = time.Now()
 
-	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice shows", eyes, "eyes")
+	eyesFiles, _ := os.Create("eyes.txt")
+	defer eyesFiles.Close()
+
+
+	dice, _ := os.Create("dice.log")
+	defer dice.Close()
 
 	// TODO: use fmt.Fprintln instead!
-	fmt.Println("the dice was rolled at", when)
+	fmt.Fprintln(os.Stdout, "the dice shows", eyes, "eyes")
+
+	// TODO: use fmt.Fprintln instead!
+	fmt.Fprintln(os.Stdout, "the dice was rolled at", when)
 
 	// TODO: how to write the output into eyes.txt and dice.log?
+
 	// go run ex3/main.go TODO
 }
